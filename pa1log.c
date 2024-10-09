697ed25b6b1d0c2d107004088ddb926d6b3530f1
Andrey1408 <andrey20034ad@gmail.com>
2024-10-09 05:20:14 +0300

    + ipc

--- pa1log.c
+++ pa1log.c
@@ -11,27 +11,27 @@ static const char * const log_pipe_open_fmt = "Pipe from process %d to %d was op
 
 
 void log_started(FILE *events_log_file, local_id process_id) {
-    printf(log_started_fmt, id, getpid(), getppid());
+    printf(log_started_fmt, process_id, getpid(), getppid());
     fprintf(events_log_file, log_started_fmt, process_id, getpid(), getppid());
 }
 
 void log_received_all_started(FILE *events_log_file, local_id process_id) {
-    printf(log_received_all_started_fmt, id);
+    printf(log_received_all_started_fmt, process_id);
     fprintf(events_log_file, log_received_all_started_fmt, process_id);
 }
 
 void log_done(FILE *events_log_file, local_id process_id) {
-	printf(log_done_fmt, id);
+	printf(log_done_fmt, process_id);
     fprintf(events_log_file, log_done_fmt, process_id);
 }
 
 void log_received_all_done(FILE *events_log_file, local_id process_id) {
-	printf(log_received_all_done_fmt, id);
+	printf(log_received_all_done_fmt, process_id);
     fprintf(events_log_file, log_received_all_done_fmt, process_id);
 }
 
 void log_pipe(FILE *pipes_log_file, local_id from, local_id to, int read, int write) {
-	fprintf(pipes_log_file, log_pipe_created_fmt, from, to, read, write);
+	fprintf(pipes_log_file, log_pipe_open_fmt, from, to, read, write);
 }
 
 
--- pipe_util.h
+++ pipe_util.h
@@ -3,12 +3,12 @@
 #include "ipc.h"
 
 typedef struct {
-    int read;
-    int write;
+    int reader;
+    int writer;
 } pipe_str;
 
 typedef struct {
     local_id cur_id;
-    size_t N;
-    
+    local_id count_p;
+    pipe_str **pipe;
 } Process;
