c40c3a72d850682a84b0fdbb22ad4f601d425643
Andrey1408 <andrey20034ad@gmail.com>
2024-10-10 03:03:40 +0300

    f

--- ipc.c
+++ ipc.c
@@ -3,7 +3,7 @@
 #include "pipe_util.h"
 
 int send(void * self, local_id dst, const Message * msg) {
-    Process *proc = self;
+    Process *proc = (Process *)self;
     pipe_str pp = proc->pipe[proc->cur_id][dst];
     if (write(pp.writer, msg, sizeof(Message)) < 0) {
         return 1;
@@ -15,7 +15,7 @@ int send(void * self, local_id dst, const Message * msg) {
 }
 
 int send_multicast(void * self, const Message * msg) {
-    Process *proc = self;
+    Process *proc = (Process *)self;
     for (local_id i = 0; i < proc->count_p; i++) {
         if (send(self, i, msg)) {
             return 1;
@@ -25,7 +25,7 @@ int send_multicast(void * self, const Message * msg) {
 }
 
 int receive(void * self, local_id from, Message * msg) {
-    Process *proc = self;
+    Process *proc = (Process *)self;
     pipe_str pp = proc->pipe[from][proc->cur_id];
     if (read(pp.reader, msg, sizeof(Message)) < 0) {
         return 1;
